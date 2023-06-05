0cf2acb5f2a9875adcb51f7a647a1afeb580d70b
davyke <chombaalex2019@gmail.com>
2023-06-05 12:42:22 +0100

    2nd commit no navbar and hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,13 @@
-import { useRef } from 'react'
+
 import './App.css'
-import Navbar from './Componets/Navbar'
-import ConferenceScheduler from './Componets/LocalConfrence';
-import LocoConferenceScheduler from './Componets/ConferenceScheduler';
 import Conference from './Componets/ConferenceScheduler';
 
-
-
 function App() {
-  const ref = useRef(null);
-
-  const options = {
-    smooth: true,
-  } 
-
-  
-
   return (
     
     
       <div className="container">
-      
-      
       <Conference/>
-
-      
-        
       </div>
     
   
