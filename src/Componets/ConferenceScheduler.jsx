0e5d19461fcaad7b178459a297b07980e0dcd9de
davyke <chombaalex2019@gmail.com>
2023-06-05 12:38:14 +0100

    second commit

--- src/Componets/ConferenceScheduler.jsx
+++ src/Componets/ConferenceScheduler.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { DateTime, Duration, Interval } from 'luxon';
-import "../app.css"
+import "../App.css"
+
 
 const Conference = () => {
   const [sessions, setSessions] = useState([]);
